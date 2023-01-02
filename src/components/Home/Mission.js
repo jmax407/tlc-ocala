ade1b181ce9cd2f36acd0782082840f417d3d3f1
Jmax <jmax407@gmail.com>
2023-01-02 08:15:31 -0500

    minor fixes

--- src/components/Home/Mission.js
+++ src/components/Home/Mission.js
@@ -20,21 +20,22 @@ const Mission = () => {
               <div className='row'>
                 <div className='col-lg-12'>
                   <div className='box-container bg-white box-shadow mb-4 borderRadius10'>
-                  <i className="fa-solid fa-hand-holding-heart 2x"></i>
-                    <h4>By Witnessing</h4>
+                  <i className="fa-solid fa-hand-holding-heart icon2x font-maroon"></i>
+                    <h4 className='font-maroon'>By Witnessing</h4>
                     <p>Outreach to Proclaim and invite to Jesus Christ.</p>
                   </div>
                 </div>
                 <div className='col-lg-12'>
                   <div className='box-container bg-white box-shadow mb-4 borderRadius10'>
-                  <i class="fa-solid fa-church 2x"></i>
-                    <h4>By Nurturing</h4>
+                  <i class="fa-solid fa-church icon2x font-maroon"></i>
+                    <h4 className='font-maroon'>By Nurturing</h4>
                     <p>Ministries to edify and build the body of Christ.</p>
                   </div>
                 </div>
                 <div className='col-lg-12'>
                   <div className='box-container bg-white box-shadow borderRadius10'>
-                    <h4>By Worshipping</h4>
+                  <i class="fa-solid fa-hands-praying icon2x font-maroon"></i>
+                    <h4 className='font-maroon'>By Worshipping</h4>
                     <p>
                       Gathering corporately in praise to the true and Living God
                       of our salvation through Jesus Christ.
